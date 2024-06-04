5b897885e52a77d64c2c6bb17e138a593b956d36
Dusuan <XxDusuanxX@hotmail.com>
2024-06-03 22:00:52 -0700

    Muy orgulloso de entender como funciona el flexbos en react native por mi cuenta propia jkasdjkf y pues muchas cosas que no le habia hecho comit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={styles.titulo}> React Native</Text> 
+      <Text>Sign in into your account</Text>
+
+
+      <TextInput style={styles.input} placeholder="JhonDoe@gmail.com" />
+      <TextInput style={styles.input}placeholder="Password" />
+
+
+
+      <StatusBar style="auto"/>
+    </View> // view son divs  y text son p
+
+
+
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontWeight: "bold",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  titulo: {
+    fontSize: 60,
+  },
+
+  input: {
+    width: 200,
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+
+
+
+
+
+
 });
